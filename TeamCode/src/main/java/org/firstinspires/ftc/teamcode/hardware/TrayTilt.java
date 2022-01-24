33f1b7767e70963a225bf3777b825f53a1682343
Amaan Mahammad <amul.mahammad@gmail.com>
2022-01-23 17:47:56 -0800

    Add degrees to servo position function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/TrayTilt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/TrayTilt.java
@@ -6,10 +6,17 @@ public class TrayTilt {
     Servo trayTilt;
     Position currentMode;
 
+    public static double degreesToServoPos(double angle) {
+        return angle / 180.0;
+    }
+
     public enum Position {
-        INTAKING(0.0),
-        SLIDING(0.1),
-        OUTTAKING(0.5);
+//        INTAKING(0.0),
+//        SLIDING(0.1),
+//        OUTTAKING(0.5);
+        INTAKING(0),
+        SLIDING(degreesToServoPos(30)),
+        OUTTAKING(degreesToServoPos(130));
 
         public double tiltPosition;
 
@@ -21,7 +28,6 @@ public class TrayTilt {
 
     public TrayTilt(DeviceManager deviceManager){
         trayTilt = deviceManager.trayTilt;
-
         trayTilt.setPosition(0);
     }
 
